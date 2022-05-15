3d25d9b145ee1d9794eed4757a5938e1abcb9531
lanlantu <zhangxi_830@163.com>
2022-05-15 22:14:11 +0800

    remove

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -35,11 +35,11 @@
         </div>
       </div>
       <!-- 向下滚动 -->
-      <div class="scroll-down" @click="scrollDown">
+      <!-- <div class="scroll-down" @click="scrollDown">
         <v-icon color="#fff" class="scroll-down-effects">
          aiaiai 
         </v-icon>
-      </div>
+      </div> -->
     </div>
     
   </div>
@@ -61,12 +61,7 @@ computed: {
     },
 },
 methods:{
-   scrollDown() {
-      window.scrollTo({
-        behavior: "smooth",
-        top: document.documentElement.clientHeight
-      });
-    },
+ 
 }
 }
 </script>
