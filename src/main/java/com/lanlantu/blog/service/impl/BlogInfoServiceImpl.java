526ef3d7814b64376d61a60c0c82de1befa0b77a
lanlantu <zhangxi_830@163.com>
2022-06-10 21:04:46 +0800

    bloginfo

--- src/main/java/com/lanlantu/blog/service/impl/BlogInfoServiceImpl.java
+++ src/main/java/com/lanlantu/blog/service/impl/BlogInfoServiceImpl.java
@@ -1,16 +1,22 @@
 package com.lanlantu.blog.service.impl;
 
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.lanlantu.blog.service.BlogInfoService;
+import com.lanlantu.blog.service.RedisService;
 import com.lanlantu.blog.util.IpUtils;
 import eu.bitwalker.useragentutils.Browser;
 import eu.bitwalker.useragentutils.OperatingSystem;
 import eu.bitwalker.useragentutils.UserAgent;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+
+import static com.lanlantu.blog.constant.CommonConst.*;
+import static com.lanlantu.blog.constant.RedisPrefixConst.*;
 
 
 @Service
@@ -21,20 +27,41 @@ public class BlogInfoServiceImpl implements BlogInfoService {
     @Resource
     private HttpServletRequest request;
 
+    @Autowired
+    private RedisService redisService;
+
 
     @Override
     public void report() {
         // 获取ip
         String ipAddress = IpUtils.getIpAddress(request);
-        log.info("ip {}",ipAddress);
         // 获取访问设备
         UserAgent userAgent = IpUtils.getUserAgent(request);
-        log.info("userAgent {}",userAgent);
         Browser browser = userAgent.getBrowser();
         OperatingSystem operatingSystem = userAgent.getOperatingSystem();
         // 生成唯一用户标识
         String uuid = ipAddress + browser.getName() + operatingSystem.getName();
+        System.out.println(uuid);
         String md5 = DigestUtils.md5DigestAsHex(uuid.getBytes());
+        System.out.println(md5);
+
+        // 判断是否访问
+        if (!redisService.sIsMember(UNIQUE_VISITOR, md5)) {
+            // 统计游客地域分布
+            String ipSource = IpUtils.getIpSource(ipAddress);
+            if (StringUtils.isNotBlank(ipSource)) {
+                ipSource = ipSource.substring(0, 2)
+                        .replaceAll(PROVINCE, "")
+                        .replaceAll(CITY, "");
+                redisService.hIncr(VISITOR_AREA, ipSource, 1L);
+            } else {
+                redisService.hIncr(VISITOR_AREA, UNKNOWN, 1L);
+            }
+            // 访问量+1
+            redisService.incr(BLOG_VIEWS_COUNT, 1);
+            // 保存唯一标识
+            redisService.sAdd(UNIQUE_VISITOR, md5);
+        }
     }
 
 
